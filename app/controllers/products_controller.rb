e0ad6fffb2e4f94921663dead6ed8ed9f11b585d
Scott C. Reynolds <scott@scottcreynolds.com>
2016-02-21 22:11:34 -0500

    solution

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,6 +22,15 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
+  end
+
   private
 
   def product_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  resources :products, only: [:index, :new, :create]
+  resources :products, only: [:show, :index, :new, :create]
   get '/products/:id/description', to: 'products#description'
   get '/products/:id/inventory', to: 'products#inventory'
+  get '/products/:id/data', to: 'products#data'
 end
