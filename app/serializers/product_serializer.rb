3ebf46aaca327db577c48f904b313e4a9f417149
Zachary Lehmann <zlehmann@gmail.com>
2019-04-16 23:37:26 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -7,7 +7,7 @@ class ProductSerializer
     serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"price": "' + product.price.to_s + '", '
     serialized_product += '"description": "' + product.description + '", '
-    serialized_product += '"inventory": "' + product.inventory.to_s + '", '
+    serialized_product += '"inventory": "' + product.inventory.to_s + '"'
 
     # and end with the close brace
     serialized_product += '}'
